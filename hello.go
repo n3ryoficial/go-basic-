2fc6fb818df346fc881d2ef9e28669aea8a5bb9a
n3ryoficial <126675401+n3ryoficial@users.noreply.github.com>
2024-08-09 16:24:15 -0300

    Update hello.go

--- hello.go
+++ hello.go
@@ -11,4 +11,16 @@ func main() {
 	var idade int = 20
 	fmt.Println("sua idade é :", idade)
 
+	//função scanf é como um ponteiro para a variavel que guardara a entrada do usuario
+
+	fmt.Println("1- iniciar monitoramento ")
+	fmt.Println("2- exibir ")
+	fmt.Println("0- exit ")
+	var comando int
+	fmt.Scanf("%d", &comando)
+
+	fmt.Println("o comando escolido foi..", comando)
+
+	
+
 }
