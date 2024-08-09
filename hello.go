415138d79ff35f849c1e5a80edf06016fc7321d7
n3ryoficial <126675401+n3ryoficial@users.noreply.github.com>
2024-08-09 15:49:24 -0300

    Add files via upload

--- hello.go
+++ hello.go
@@ -3,6 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("ola mundo!")
+	//colocando  varivel (string)
+	var nome string = "Nery"
+	fmt.Println("Ola sr.", nome)
+
+	//colocando a varievel int(nummero inteiro)
+	var idade int = 20
+	fmt.Println("sua idade é :", idade)
 
 }
