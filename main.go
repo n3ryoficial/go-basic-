b9b8398ad58b8352285e256f8c5a728cf1d3e4dc
n3ryoficial <126675401+n3ryoficial@users.noreply.github.com>
2024-09-23 18:16:50 -0300

    Update main.go

--- main.go
+++ main.go
@@ -8,10 +8,14 @@ type ContaCorrente struct {
 	numeroAgencia int
 	numeroConta   int
 	saldo         float64
+	senha         string
 }
 
 // metedo para sacar dinheiro
-func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+func (c *ContaCorrente) Sacar(valorDoSaque float64, senha string) string {
+	if senha != c.senha {
+		return "Assinatura digital incorreta"
+	}
 	if valorDoSaque > 0 && valorDoSaque <= c.saldo {
 		c.saldo -= valorDoSaque
 		return "Saque realizado com sucesso!"
@@ -20,7 +24,7 @@ func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
 	}
 }
 
-// metodo para desito
+// metodo para deposito
 func (c *ContaCorrente) Depositar(valorDoDeposito float64) string {
 	if valorDoDeposito > 0 {
 		c.saldo += valorDoDeposito
@@ -30,20 +34,49 @@ func (c *ContaCorrente) Depositar(valorDoDeposito float64) string {
 	}
 }
 
+// metodo de tranferir dinheiro para outra conta
+func (c *ContaCorrente) tranferir(valor float64, outraConta *ContaCorrente, senha string) string {
+	if senha != c.senha {
+		return "Assinatura digital incorreto"
+	}
+	if valor > 0 && valor <= c.saldo {
+		c.saldo -= valor
+		outraConta.saldo += valor
+		return "Trasferencia relizada com sucesso!"
+	} else {
+		return "Saldo insuficiente ou valor incorreto"
+	}
+}
+
 func main() {
-	contaDoMatheus := ContaCorrente{titular: "Matheus",
-		numeroAgencia: 589, numeroConta: 5312, saldo: 125.5}
+	contaDoMatheus := ContaCorrente{
+		titular:       "Matheus",
+		numeroAgencia: 589,
+		numeroConta:   5312,
+		saldo:         125.5,
+		senha:         "senha75",
+	}
 	//como nao presisa de cc cv só nessecita escrever os valores observe
 
-	contaDaRosi := ContaCorrente{"Rosi", 415, 1745, 1234.5}
+	contaDaRosi := ContaCorrente{
+		titular:       "Rosi",
+		numeroAgencia: 415,
+		numeroConta:   1745,
+		saldo:         1324.5,
+		senha:         "senha76",
+	}
 	fmt.Println(contaDoMatheus)
 	fmt.Println(contaDaRosi)
-	// sacar
-	fmt.Println(contaDoMatheus.Sacar(50))
-	fmt.Println(contaDoMatheus.saldo)
 
-	// depositar
-	fmt.Println(contaDaRosi.Depositar(300))
-	fmt.Println(contaDaRosi.saldo)
+	// saque com senha correta
+	fmt.Println("\nsaque com senha correta:")
+	fmt.Println(contaDoMatheus.Sacar(25, "senha75"))
+	fmt.Println("saldo de Matheus ", contaDoMatheus.saldo)
+
+	//transferencia com senha correta
+	fmt.Println("\n tranferencia com senha correta:")
+	fmt.Println(contaDoMatheus.tranferir(50, &contaDaRosi, "senha76"))
+	fmt.Println("saldo de Matheus:", contaDoMatheus.saldo)
+	fmt.Println("saldo de Rosi:", contaDaRosi.saldo)
 
 }
